424d00127b04bccb44d7511d2fdae631f04bfdc8
freddicola <freddicola@gmail.com>
2022-02-03 22:28:58 +0900

    plotly test

--- src/pages/page1.vue
+++ src/pages/page1.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="row" :style="content_height" ref="row2">
         <div class="col-5 bg-grey-8">c</div>
-        <div class="col-7 bg-yellow-3">d</div>
+        <div class="col-7 bg-yellow-3" ref="plane">d</div>
       </div>
     </div>
   </q-page>
@@ -22,6 +22,8 @@
 </style>
 
 <script>
+import Plotly from "plotly.js-dist-min";
+
 export default {
   name: "Page1",
   data() {
@@ -38,6 +40,44 @@ export default {
     },
   },
   computed: {},
-  mounted: function () {},
+  mounted: function () {
+    const plane = this.$refs.plane;
+    const x = [
+      "2020-02",
+      "2020-03",
+      "2020-04",
+      "2020-05",
+      "2020-06",
+      "2020-07",
+      "2020-08",
+    ];
+    const y = [10.3, 15.2, 15.9, 10.2, 11.3, null, 12.3];
+
+    const series = {
+      x: x,
+      y: y,
+      mode: "lines+markers",
+      connectgaps: true,
+    };
+
+    const threshold = {
+      x: ["2020-01", "2020-09"],
+      y: [9.0, 9.0],
+      mode: "lines",
+      connectgaps: true,
+    };
+
+    const layout = {
+      xaxis: {
+        type: "date",
+      },
+      yaxis: {
+        range: [0.0, 16.0],
+        dtick: 3.0,
+      },
+    };
+
+    Plotly.newPlot(plane, [series, threshold], layout);
+  },
 };
 </script>
